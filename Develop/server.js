863cb157177205bddb2d0151878820a97cb37ecf
Richard Martin <richie@Richards-MacBook-Air.local>
2022-11-13 20:04:36 -0600

    added path to notes

--- Develop/server.js
+++ Develop/server.js
@@ -1,13 +1,19 @@
-const express = ('express');
+const express = require ('express');
 const app = express();
 const PORT = 3001;
 
-app.get('*',function (routeHandler) {  })
+app.get('*',function routeHandler(req,res) { 
+    res.send('ok');
+ });
+ app.use(express.static('/'))
+ app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
+ 
+ app.get('/send', (req, res) =>
+   res.sendFile(path.join(__dirname, 'public/notes.html'))
+ );
 
 
 
-
-
-app.get(req, res, () => {
-    res.send('http://localhost/3001')
-})
+// app.get(req, res, () => {
+//     res.send('http://localhost/3001')
+// })
