e3ef366f424bd3dd24c2d22939c7d85e98a7bc62
Richard Martin <richie@Richards-MacBook-Air.local>
2022-11-13 19:48:25 -0600

    Uploaded challenge into new repo with some changes already

---
+++ Develop/server.js
@@ -0,0 +1,13 @@
+const express = ('express');
+const app = express();
+const PORT = 3001;
+
+app.get('*',function (routeHandler) {  })
+
+
+
+
+
+app.get(req, res, () => {
+    res.send('http://localhost/3001')
+})
