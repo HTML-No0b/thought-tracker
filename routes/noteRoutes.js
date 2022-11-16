1dd2d51d609d9d7f54e953650074c7c727b3637d
Richard Martin <richie@Richards-MacBook-Air.local>
2022-11-15 23:01:35 -0600

    tried to add a post method, but ended with failure

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,18 +1,28 @@
 const noteRoutes = require('express').Router();
 const fs = require('fs');
 const path = require('path')
-const dbJson = require('../db/db.json')
+const dbcopy = require('../db/db.json')
 // const db = require('/Develop/db/db.json')
-noteRoutes.get('/notes',(req,res)=>{
-    const dbcopy = JSON.parse(fs.readFileSync('./db/db.json','UTF-8'));
-    console.log(dbcopy)
-    res.json(dbcopy)
+noteRoutes.get('/notes', (req, res) => {
+  const dbcopy = JSON.parse(fs.readFileSync('/db/db.json', 'UTF-8'));
+  console.log(dbcopy)
+  res.json(dbcopy)
 
-    app.post('/notes', (req, res) => {
-    req.body.id 
-  
+  app.post('/notes', (req, res) => {
+    let db = fs.readFileSync('../db/db.json');
+    db = JSON.parse(db);
+    res.json(db);
+    let userNote = {
+      title: req.body.title,
+      text: req.body.text,
+      id: uniqid(),
+
+    };
+    db.push(userNote);
+    fs.writeFileSync('/db/db.json', JSON.stringify(db));
+    res.json(db)
   });
 });
 
 
-module.exports = noteRoutes
+  module.exports = noteRoutes
