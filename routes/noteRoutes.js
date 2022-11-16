be85ef971d6a5248ddf566173a9fa0336e1a9777
Richard Martin <richie@Richards-MacBook-Air.local>
2022-11-15 23:13:37 -0600

    fixed some problem with the db read

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,28 +1,18 @@
 const noteRoutes = require('express').Router();
 const fs = require('fs');
 const path = require('path')
-const dbcopy = require('../db/db.json')
+const dbJson = require('../db/db.json')
 // const db = require('/Develop/db/db.json')
-noteRoutes.get('/notes', (req, res) => {
-  const dbcopy = JSON.parse(fs.readFileSync('/db/db.json', 'UTF-8'));
-  console.log(dbcopy)
-  res.json(dbcopy)
+noteRoutes.get('/notes',(req,res)=>{
+    const dbcopy = JSON.parse(fs.readFileSync('./db/db.json','UTF-8'));
+    console.log(dbcopy)
+    res.json(dbcopy)
 
-  app.post('/notes', (req, res) => {
-    let db = fs.readFileSync('../db/db.json');
-    db = JSON.parse(db);
-    res.json(db);
-    let userNote = {
-      title: req.body.title,
-      text: req.body.text,
-      id: uniqid(),
-
-    };
-    db.push(userNote);
-    fs.writeFileSync('/db/db.json', JSON.stringify(db));
-    res.json(db)
+    app.post('/notes', (req, res) => {
+    req.body.id 
+  
   });
 });
 
 
-  module.exports = noteRoutes
+module.exports = noteRoutes
