db371f500c7631ff43391d2dd02d35d8137fcf9d
Richard Martin <richie@Richards-MacBook-Air.local>
2022-11-15 21:39:42 -0600

    added heroku

--- Develop/server.js
+++ Develop/server.js
@@ -3,29 +3,32 @@ const app = express();
 const PORT = 3001;
 const noteRoutes = require('./routes/noteRoutes')
 const path = require('path')
-// const htmlRoutes = require('./routes/htmlRoutes')
+const allNotes = require('./db/db.json');
+const { response, application } = require('express');
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-
 app.use(express.static(__dirname + '/public'));
+
+
 console.log(__dirname)
-
-
-// app.get('/send', (req, res) =>
-//   res.sendFile(path.join(__dirname, 'public/notes.html'))
-// );
 app.use('/api', noteRoutes)
-// app.use('/', htmlRoutes)
 app.get('/', (req, res) => res.sendFile((path.join(__dirname, '/public/index.html'))));
 app.get('/notes', (req, res) => res.sendFile((path.join(__dirname, '/public/notes.html'))));
 
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 
+});
+
+
+app.post('/notes', (req, res) => {
+  req.body.id 
 
 });
+
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
